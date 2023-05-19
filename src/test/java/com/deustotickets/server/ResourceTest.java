eb744b96e7a72a0ee7072dac548ea8daf10e5c58
Asier Bujedo Álvarez <90905570+AsierBujedo@users.noreply.github.com>
2023-05-19 17:28:10 +0200

    Corrección de un error en ResourceTest con Mockito
    Co-Authored-By: Iker López <91015199+ikerLopezZ@users.noreply.github.com>

--- src/test/java/com/deustotickets/server/ResourceTest.java
+++ src/test/java/com/deustotickets/server/ResourceTest.java
@@ -1,10 +1,16 @@
 package com.deustotickets.server;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Invocation;
@@ -20,31 +26,55 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import com.deustotickets.dao.ArtistaDAO;
+import com.deustotickets.dao.ConciertoDAO;
+import com.deustotickets.dao.EntradaDAO;
 import com.deustotickets.dao.UsuarioDAO;
-import com.deustotickets.domain.TipoUsuario;
-import com.deustotickets.domain.Usuario;
+import com.deustotickets.domain.*;
+
 
 public class ResourceTest {
+	
+	Usuario u;
+	
+	private Usuario usuario;
 
-    @Mock
     private UsuarioDAO usuarioDAO;
-    @Mock
+    
+    private ConciertoDAO conciertoDAO;
+    
+    private EntradaDAO entradaDAO;
+    
+    private ArtistaDAO artistaDAO;
+    
     private Resource resource;
+    
+    private Response response;
     
     @Before
     public void setUp() {
-      MockitoAnnotations.openMocks(this);
-    }
+      usuario = mock(Usuario.class);
+      usuarioDAO = mock(UsuarioDAO.class);
+      conciertoDAO = mock(ConciertoDAO.class);
+      entradaDAO = mock(EntradaDAO.class);
+      artistaDAO = mock(ArtistaDAO.class);
+      resource = mock(Resource.class);
+      response = mock(Response.class);
+      u = new Usuario("test", "test@example.com", "password", TipoUsuario.CLIENTE);
+      
+      usuario.setNombreApellidos(u.getNombreApellidos());
+      usuario.setEmail(u.getEmail());
+      usuario.setPassword(u.getPassword());
+      usuario.setTipo(u.getTipo());
+      }
 
     @Test
     public void testLoginUserSuccessful() throws Exception {
-//        Usuario u = new Usuario("Test", "test@example.com", "password", TipoUsuario.CLIENTE);
-//
-//        when(usuarioDAO.find(eq(u.getEmail()))).thenReturn(u);
-//
-//        Response response = resource.loginUser(u);
-//
-//        assertEquals(200, response.getStatus());
+    	
+    	when(usuarioDAO.find(u.getEmail())).thenReturn(u);
+    	
+    	assertEquals(Resource.loginUser(u).getStatus(), Response.Status.OK.getStatusCode());
     }
 
     @Test
@@ -69,4 +99,31 @@ public class ResourceTest {
 //
 //        assertEquals(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), response.getStatus());
     }
+    
+    @Test
+    public void testgetConcerts() throws Exception {
+    	ArrayList<Concierto> lista = new ArrayList<Concierto>();
+    	lista.add(new Concierto("Test", null, "Test","Test" , 110));
+    	
+    	when(conciertoDAO.getAll()).thenReturn(lista);
+    
+    	assertEquals(Resource.getConcerts().getStatus(), Response.Status.OK.getStatusCode());
+    }
+    
+//    @Test
+//    public void testgetConcertsException() throws Exception {
+//    	when(conciertoDAO.getAll()).thenThrow(new Exception("TEST"));
+//        
+//    	assertThrows(Exception.class,() -> Resource.getConcerts());
+//    }
+    
+    @Test
+    public void testChangeUsername() throws Exception {
+    	when(usuarioDAO.find(u.getEmail())).thenReturn(u);
+    	
+    	assertEquals(Resource.changeUsername(u).getStatus(), Response.Status.OK.getStatusCode());
+    }
+    
+    
+    
 }
